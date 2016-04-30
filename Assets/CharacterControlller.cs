0e67e74b15feecab2e9108254b5ea77c24e8d8e6
João Gabriel Jardim <jgabriel.jardim@gmail.com>
2016-04-30 17:53:04 -0300

    Criado movimentação de personagem utilizando as teclas 'a', 'd', 'w', 's'.

---
+++ Assets/CharacterControlller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControlller : MonoBehaviour {
+
+	public float speed = 2.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horizontalMove = Input.GetAxis ("Horizontal") * speed;
+		float verticalMove = Input.GetAxis ("Vertical") * speed;
+
+		horizontalMove *= speed * Time.deltaTime;
+		verticalMove *= speed * Time.deltaTime;
+		transform.Translate (horizontalMove, 0, verticalMove);
+	
+	}
+}
