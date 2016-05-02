3dea74068f685da7c92e319c08981877a589ce39
João Gabriel Jardim <jgabriel.jardim@gmail.com>
2016-05-02 00:34:34 -0300

    Adicionado algumas classes faltando. Movido scrips para pasta Scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int life;
+	public int startLife = 100;
+	public int basicDamage = 10;
+
+
+	public void OnCollisionEnter(Collision col)
+	{
+		Debug.Log (col.gameObject.name);
+
+		if (col.gameObject.name == "Shot") {
+			life -= basicDamage;
+			Debug.Log ("Life amount = " + life);
+		} else
+			Debug.Log ("Not shot");
+	}
+}
